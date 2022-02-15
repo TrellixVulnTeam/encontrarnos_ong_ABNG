7e4e3d30ee0946cb813f65931bfc64b8513084f2
María Belén Trigueros <bel.trigueros@gmail.com>
2022-02-15 15:16:11 -0300

    Display Taxonomies name done

--- wp-content/themes/encontrarnos_theme/archive-organismos.php
+++ wp-content/themes/encontrarnos_theme/archive-organismos.php
@@ -56,30 +56,58 @@ $termz = get_term_by( 'id', $child, 'tipos-de-organismo' ); ?>
 
     let select = $('#select-organismos').val();
     let select_name = $('#select-organismos').find('option:selected').text();
+
 	let endpoint = '<?php echo admin_url('admin-ajax.php');?>'
 	
-	console.log(select);
-
-    console.log(select_name);
-            let data = select.split(",");
-            let sel = document.getElementById('select-provincias');
-            let newClassname = select_name.toLowerCase() + '--done';
+    let data = select.split(",");
+    let sel = document.getElementById('select-provincias');
+    let newClassname = select_name.toLowerCase() + '--done';
+            
             if(!sel.classList.contains(newClassname)){
+                append_options(); 
+            }
+            
+
+            let provinciales = document.getElementsByClassName('provinciales');
+            let ongs = document.getElementsByClassName('ongs');
+
+            switch(select_name){
+                    case "Nacionales":
+                        $(provinciales).addClass("display--n");
+                        $(ongs).addClass("display--n");
+                        break;
+                    case "Provinciales":
+                        $(provinciales).addClass("display--b");
+                        $(ongs).addClass("display--n");
+                        $(provinciales).removeClass("display--n");
+                        $(ongs).removeClass("display--b");
+                    break;
+                    case "ONGs":
+                        $(ongs).addClass("display--b");
+                        $(provinciales).addClass("display--n");
+                        $(provinciales).removeClass("display--b");
+                        $(ongs).removeClass("display--n");
+                    break;
+                    default: 
+                        $(provinciales).addClass("display--n");
+                        $(ongs).addClass("display--n");
+                    break;
+            }
+
+            function append_options(){
+                if(!sel.classList.contains(newClassname)){
                 for (let i= 0; i< data.length-1; i++) {
                 let opt = document.createElement('option');
                 opt.className = select_name.toLowerCase();
                 opt.text = data[i];
                 sel.classList.add(newClassname);
-                sel.appendChild(opt);                  
-                if(select_name.toLowerCase() == opt.className){
-                    opt.style.display = "block";}
-                    else {
-                        opt.style.display = "none";
-                    }
-            }
-            }
+                sel.appendChild(opt);           
 
-})
+                }
+            }    
+        }
+    })
+
 })(jQuery);
 
 
