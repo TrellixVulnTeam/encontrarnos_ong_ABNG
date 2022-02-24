e4468a99e0f121d2556e6aac595b1cb6730f6957
María Belén Trigueros <bel.trigueros@gmail.com>
2022-02-23 22:46:55 -0300

    Update header

--- wp-content/themes/encontrarnos_theme/header.php
+++ wp-content/themes/encontrarnos_theme/header.php
@@ -10,16 +10,15 @@
  */
 
 
-// TAMAÑO DEL LOGO
+// VARIABLES LOGO
 
 $custom_logo_id = get_theme_mod( 'custom_logo' );
 $image = wp_get_attachment_image_src( $custom_logo_id , 'logo-lg'); 
 
- $image_width = $image[1];
- $image_height = $image[2];
+$image_width = $image[1];
+$image_height = $image[2];
 
 ?>
-
 
 <!doctype html>
 <html <?php language_attributes(); ?>>
@@ -32,30 +31,35 @@ $image = wp_get_attachment_image_src( $custom_logo_id , 'logo-lg');
 
 <body <?php body_class(); ?>>
 
+<?php wp_body_open(); ?>
 
-<?php wp_body_open();
-
-?>
+<!-- NAV -->
 <nav id="site-navigation" class="main-navigation">
-<div class="header-bar">
-<div>
-<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><i class="bi bi-list"></button></i>
-</div>
-<div class= "header-rss">
-<a  href="<?php the_field('facebook', 'options');?>"><i class="bi bi-facebook"></i></a>
-<a href="<?php the_field('instagram', 'options');?>"><i class="bi bi-instagram"></i></a>
-<a class= "link-padding" href="<?php the_field('twitter', 'options');?>"><i class="bi bi-twitter"></i></a>
-</div>
-</div>
+	<!-- HEADER TOP BAR -->
+	<div class="header-bar">
+		<!-- MENU MOBILE -->
+		<div>
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><i class="bi bi-list"></button></i>
+		</div>
+		<!-- END MENU MOBILE -->
+		<!-- RSS -->
+		<div class= "header-rss">
+			<a  href= "<?php the_field('facebook', 'options');?>"><i class="bi bi-facebook"></i></a>
+			<a href= "<?php the_field('instagram', 'options');?>"><i class="bi bi-instagram"></i></a>
+			<a class= "link-padding" href="<?php the_field('twitter', 'options');?>"><i class="bi bi-twitter"></i></a>
+		</div>
+		<!-- END RSS -->
+	</div>
+	<!-- END HEADER TOP BAR -->
 
-<div class="content-wrapper">
-	<div id="page" class="site">
-		<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'encontrarnos_theme' ); ?></a>
-
-		<header id="masthead" class="site-header">
-
-			<figure class= "logo--menu"><img src= "<?php echo $image[0] ?>" width= "<?php echo $image_width ?>" height= "<?php echo $image_height ?> "></figure>
-
+	<!-- CONTENT WRAPPER -->
+	<div class="content-wrapper">
+		<div id="page" class="site">
+			<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'encontrarnos_theme' ); ?></a>
+			<header id="masthead" class="site-header">
+				<!-- LOGO -->
+				<figure class= "logo--menu"><img src= "<?php echo $image[0] ?>" width= "<?php echo $image_width ?>" height= "<?php echo $image_height ?> "></figure>
+				<!-- END LOGO -->
 				<?php
 				wp_nav_menu(
 					array(
@@ -65,6 +69,6 @@ $image = wp_get_attachment_image_src( $custom_logo_id , 'logo-lg');
 					)
 				);
 				?>
-			</nav><!-- #site-navigation -->
-
-		</header><!-- #masthead -->
+		</nav>
+			<!-- END NAV -->
+</header>
