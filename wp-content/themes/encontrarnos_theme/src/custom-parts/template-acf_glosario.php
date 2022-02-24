7bcc8e77888575ec2806709826b12c3c3c33cfd1
María Belén Trigueros <bel.trigueros@gmail.com>
2022-02-23 23:47:16 -0300

    Update Glosario

--- wp-content/themes/encontrarnos_theme/src/custom-parts/template-acf_glosario.php
+++ wp-content/themes/encontrarnos_theme/src/custom-parts/template-acf_glosario.php
@@ -1,7 +1,5 @@
-<?php if(have_rows('glosario')): ?>
-    <?php while(have_rows('glosario')): the_row(); ?>
+<?php if(have_rows('glosario')): while(have_rows('glosario')): the_row(); ?>
         <p class= "table__item-1"><?php the_sub_field('termino');?></p>
         <p class= "table__item-2"><?php the_sub_field('abreviatura');?></p>
         <p class= "table__item-3"><?php the_sub_field('descripcion');?></p>
-    <?php endwhile; ?>
-    <?php endif; ?>
+<?php endwhile; endif; ?>
--- wp-content/themes/encontrarnos_theme/template-glosario.php
+++ wp-content/themes/encontrarnos_theme/template-glosario.php
@@ -6,71 +6,77 @@ Template Name: Glosario
 get_header();
 ?>
 
-
-
 </div>
 </div>
-
+<!-- END CONTENT WRAPPER -->
 
 <?php 
+
+// VARIABLES HERO
 $img_data = get_field('imagen-g', 'options');
 $img_url = $img_data['sizes']['hero-img'];
 $img_width = $img_data['sizes']['hero-img-width'];
 $img_height = $img_data['sizes']['hero-img-height'];
 $img_alt = $img_data['alt'];
 
-
 ?> 
 
-	
+<!-- HERO -->
+
+<!-- HERO BACKGROUND  -->
 <div class="hero__bg">
-<div class="content-wrapper hero">
-<div class="hero__text">
-<h1 class= "title title--left"><?php the_field('titulo-g', 'options'); ?></h1>
-<?php the_field('descripcion-g', 'options'); ?>
-</div>
-
-<img class= 'hero__img' src= '<?php echo $img_url; ?> ' width= " <?php echo $img_width;?>" height= '<?php echo $img_height ; ?>' alt= '<?php echo $img_alt ?>'>
-
-
-</div>
-</div>
-<div class="content-wrapper">
-
-	<main id="primary" class="site-main">
-		<?php
-		while ( have_posts() ) :
-			the_post();
-			?>
-			<div class="table">
-			<h6 class= "table__title-1">Término</h6>
-			<h6 class= "table__title-2">Abreviatura</h6>
-			<h6 class= "table__title-3">Descripción</h6>
-			<?php get_template_part( './src/custom-parts/template', 'acf_glosario' );?> 
-			</div>       
-		<?php 
-		endwhile; // End of the loop.
-		?>
-
-		<div class="glosario-wrapper">
-		<img class= "glosario__img" src='<?php
-
-		$img_data_glosario = get_field('imagen');
-		$img_url_glosario = $img_data_glosario['sizes']['img-glosario'];
-		$img_width_glosario = $img_data_glosario['sizes']['img-glosario-width'];
-		$img_height_glosario = $img_data_glosario['sizes']['img-glosario-height'];
-		$img_alt_glosario = $img_data_glosario['alt'];
-		
-		
-		echo $img_url_glosario; ?> ' width= " <?php echo $img_width_glosario;?>" height= '<?php echo $img_height_glosario; ?>' alt= '<?php echo $img_alt_glosario ?>'
-
-		
-		
-		
-		'>
+	<!-- HERO WRAPPER  -->
+	<div class="content-wrapper hero">
+		<!-- HERO TEXT WRAPPER  -->
+		<div class="hero__text">
+			<h1 class= "title title--left"><?php the_field('titulo-g', 'options'); ?></h1>
+			<?php the_field('descripcion-g', 'options'); ?>
 		</div>
+		<!-- END HERO TEXT WRAPPER -->
+		<img class= 'hero__img' src= '<?php echo $img_url; ?> ' width= " <?php echo $img_width;?>" height= '<?php echo $img_height ; ?>' alt= '<?php echo $img_alt ?>'>
+	</div>
+	<!-- END HERO WRAPPER -->
+</div>
+<!-- END HERO BACKGROUND -->
 
-	</main><!-- #main -->
+<!-- CONTENT WRAPPER -->
+<div class="content-wrapper">
+	<!-- MAIN -->
+	<main id="primary" class="site-main">
+		<!-- THE LOOP -->
+		<?php while ( have_posts() ) : the_post(); ?>
+			<!-- TABLE -->
+			<div class="table">
+				<h6 class= "table__title-1">Término</h6>
+				<h6 class= "table__title-2">Abreviatura</h6>
+				<h6 class= "table__title-3">Descripción</h6>
+				<?php get_template_part( './src/custom-parts/template', 'acf_glosario' );?> 
+			</div>       
+			<!-- END TABLE -->
+		<?php endwhile; ?>
+		<!-- END LOOP -->
+
+		<!-- GLOSARIO WRAPPER -->
+		<div class="glosario-wrapper">
+			<img class= "glosario__img" src='
+			<?php
+			
+			// VARIABLES IMG GLOSARIO
+			$img_data_glosario = get_field('imagen');
+			$img_url_glosario = $img_data_glosario['sizes']['img-glosario'];
+			$img_width_glosario = $img_data_glosario['sizes']['img-glosario-width'];
+			$img_height_glosario = $img_data_glosario['sizes']['img-glosario-height'];
+			$img_alt_glosario = $img_data_glosario['alt'];
+			// URL IMG GLOSARIO
+			echo $img_url_glosario; 
+			?> 
+			' width= " <?php echo $img_width_glosario;?>" height= '<?php echo $img_height_glosario; ?>' alt= '<?php echo $img_alt_glosario ?>'>
+		</div>
+		<!-- END GLOSARIO WRAPPER -->
+	</main>
+	<!-- EMD MAIN -->
+</div>
+<!-- END CONTENT WRAPPER -->
 
 <?php get_footer(); ?>
 
