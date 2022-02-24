3e82d131d3cd129a3dabdf7df026e8ed0be90dcf
María Belén Trigueros <bel.trigueros@gmail.com>
2022-02-24 12:39:42 -0300

    Update Home

--- wp-content/themes/encontrarnos_theme/front-page.php
+++ wp-content/themes/encontrarnos_theme/front-page.php
@@ -2,97 +2,67 @@
 
 </div>
 </div>
-
-
+<!-- END CONTENT WRAPPER -->
 
 <?php 
+
+// VARIABLES HERO
 $img_data = get_field('imagen-hp', 'options');
 $img_url = $img_data['sizes']['hero-img'];
 $img_width = $img_data['sizes']['hero-img-width'];
 $img_height = $img_data['sizes']['hero-img-height'];
 $img_alt = $img_data['alt'];
+
 ?> 
 
-<main id="primary" class="site-main">
+<!-- HERO -->
 
+<!-- HERO BACKGROUND  -->
 <div class="hero__bg">
-<div class="content-wrapper hero">
-<div class="hero__text">
-<h1 class= "title title--left"><?php the_field('titulo-hp', 'options'); ?></h1>
-<?php the_field('descripcion-hp', 'options'); ?>
+<!-- HERO WRAPPER  -->
+	<div class="content-wrapper hero">
+		<!-- HERO TEXT WRAPPER  -->
+		<div class="hero__text">
+			<h1 class= "title title--left"><?php the_field('titulo-hp', 'options'); ?></h1>
+			<?php the_field('descripcion-hp', 'options'); ?>
+		</div>
+		<!-- END HERO TEXT WRAPPER -->
+		<img class= 'hero__img' src= '<?php echo $img_url; ?> ' width= " <?php echo $img_width;?>" height= '<?php echo $img_height ; ?>' alt= '<?php echo $img_alt ?>'>
+	</div>
+	<!-- END HERO WRAPPER -->
 </div>
+<!-- END HERO BACKGROUND -->
 
-<img class= 'hero__img' src= '<?php echo $img_url; ?> ' width= " <?php echo $img_width;?>" height= '<?php echo $img_height ; ?>' alt= '<?php echo $img_alt ?>'>
-
-<!-- THE LOOP -->
-
-
-</div>
-</div>
-<div class="content-wrapper">
-
-<section>
-		<?php if(have_posts() ) : while ( have_posts() ) : the_post();?>
-        <?php endwhile;
-		else :
-			get_template_part( 'template-parts/content', 'none' );
-		endif;
-		?>
-</section>
-
-
-<!-- QUIÉNES SOMOS -->
-
-<section>
-	<h3 class="title title--left"><span class="title__span title__span--green"></span>¿Quiénes somos?</h3>
-	<?php the_field('descripcion-qs', 'options'); ?>
-	<a href= "<?php echo home_url('/equipo') ?>"><button class= "button button--green">Ver más</button></a>
-</section>
-
-<!-- PREGUNTAS FRECUENTES -->
-
-</div>
-<section>
-<div class="section-wrapper--01">
-<div class="content-wrapper section-wrapper--m section-wrapper--p">
-	<h3 class="title"><span class="title__span"></span>Preguntas Frecuentes</h3>
-	<h5 class="title title--left title--mb-0 title--mt">Sobre Búsquedas</h5>
-					<?php 
-					$args= array(
-						'post_type' => 'faqs',
-						'posts_per_page' => 2,
-						'tax_query' => array(
-							array(
-								'taxonomy' => 'tipo-de-busqueda',
-								'field' => 'slug',
-								'terms' => 'sobre-busquedas'
-
-							)
-						)
-					);
-					$_posts= new WP_Query($args);
-					?>
-
-			<?php 
-
-			if($_posts->have_posts()): 
-				while ($_posts->have_posts()) : $_posts->the_post(); ?>
-				<details>
-					<summary class= "question-wrapper"> <?php the_title();?> 
-						<i class="bi bi-chevron-compact-down"></i>
-					</summary>
-					<hr>
-					<div class="summary"> <?php the_content();?> </div>				
-				</details>
-					<hr>
-			<?php	
-				endwhile;
-			endif;
-			wp_reset_postdata(); ?>
-			
-			<a href= "<?php echo home_url('/faqs') ?>"><button class= "button button--center">Ver más</button></a>
-	<h5 class="title title--left title--mb-0">Sobre Tests y Matchs</h5>
+<!-- MAIN -->
+<main id="primary" class="site-main">
+	<!-- CONTENT WRAPPER -->
+	<div class="content-wrapper">
+	<!-- THE LOOP -->
+		<?php if(have_posts() ) : while ( have_posts() ) : the_post(); endwhile;
+		else : get_template_part( 'template-parts/content', 'none' ); endif; ?>
+	<!-- END LOOP -->
+	
+	<!-- SECTION QUIÉNES SOMOS -->
+		<section class= "mt">
+			<h3 class="title title--left"><span class="title__span title__span--green"></span>¿Quiénes somos?</h3>
+			<?php the_field('descripcion-qs', 'options'); ?>
+			<a href= "<?php echo home_url('/equipo') ?>"><button class= "button button--green">Ver más</button></a>
+		</section>
+		<!-- END SECTION QUIENES SOMOS -->
+	</div>	
+	<!-- END CONTENT WRAPPER -->
+	
+	<!-- SECTION PREGUNTAS FRECUENTES -->
+	<section class = "mt">
+		<!-- SECTION BACKGROUND -->
+		<div class="section-bg--01">
+		<!-- CONTENT WRAPPER -->
+			<div class="content-wrapper">
+				<h3 class="title"><span class="title__span"></span>Preguntas Frecuentes</h3>
+				<!-- SOBRE BUSQUEDAS -->
+				<h5 class="title title--left title--mb-0 title--mt">Sobre Búsquedas</h5>
 				<?php 
+				// QUERY SOBRE BUSQUEDAS
 				$args= array(
 					'post_type' => 'faqs',
 					'posts_per_page' => 2,
@@ -100,132 +70,177 @@ $img_alt = $img_data['alt'];
 						array(
 							'taxonomy' => 'tipo-de-busqueda',
 							'field' => 'slug',
-							'terms' => 'sobre-tests-matchs'
-
-						)
-					)
-				);
-				$_posts= new WP_Query($args);
+							'terms' => 'sobre-busquedas' )
+					) );
+				$query_sb = new WP_Query($args);
+				// END QUERY BUSQUEDAS
 				?>
-
 				<?php 
-
-				if($_posts->have_posts()): 
-					while ($_posts->have_posts()) : $_posts->the_post(); ?>
+				// CUSTOM LOOP
+				if($query_sb->have_posts()): while ($query_sb->have_posts()) : $query_sb->the_post(); ?>
+				<!-- DETAILS -->
 					<details>
-						<summary class= "question-wrapper"> <?php the_title();?> 
-							<i class="bi bi-chevron-compact-down"></i>
-						</summary>
+						<summary class= "question-wrapper"> <?php the_title();?> <i class="bi bi-chevron-compact-down"></i> </summary>
 						<hr>
-						<div> <?php the_content();?> </div>				
+						<div class="summary"> <?php the_content();?> </div>				
 					</details>
+					<!-- END DETAILS -->
 						<hr>
-				<?php	
-					endwhile;
-				endif;
+				<?php endwhile;	endif;
 				wp_reset_postdata(); ?>
-
-				<a href= "<?php echo home_url('/faqs') ?>"><button class= "button button--center">Ver más</button></a>
+				<!-- END CUSTOM LOOP -->
+					<a href= "<?php echo home_url('/faqs') ?>"><button class= "button button--center">Ver más</button></a>
+					<!-- END SOBRE BUSQUEDAS -->
+					<!-- SOBRE TESTS Y MATCHS -->
+					<h5 class="title title--left title--mb-0">Sobre Tests y Matchs</h5>
+					<?php 
+					// QUERY TESTS Y MATCHS
+					$args= array(
+						'post_type' => 'faqs',
+						'posts_per_page' => 2,
+						'tax_query' => array(
+							array(
+								'taxonomy' => 'tipo-de-busqueda',
+								'field' => 'slug',
+								'terms' => 'sobre-tests-matchs'	)
+						) );
+					$query_stm= new WP_Query($args);
+					// END QUERY TESTS Y MATCHS
+					?>
+					<?php 
+					// CUSTOM LOOP
+					if($query_stm->have_posts()): while ($query_stm->have_posts()) : $query_stm->the_post(); ?>
+					<!-- DETAILS -->
+						<details>
+							<summary class= "question-wrapper"> <?php the_title();?> <i class="bi bi-chevron-compact-down"></i>	</summary>
+							<hr>
+							<div> <?php the_content();?> </div>				
+						</details>
+						<!-- END DETAILS -->
+							<hr>
+					<?php endwhile;	endif;
+					wp_reset_postdata(); ?>
+					<!-- END CUSTOM LOOP -->
+					<a href= "<?php echo home_url('/faqs') ?>"><button class= "button button--center">Ver más</button></a>
+					<!-- END SOBRE TESTS Y MATCHS -->
+				</div>
+				<!-- END CONTENT WRAPPER -->
 			</div>
+		<!-- END SECTION BACKGROUND -->
+	</section>
+	<!-- END SECTION PREGUNTAS FRECUENTES -->
 
-</div>
-</section>
+	<?php 
+	// QUERY GLOSARIO
+	$query_glosario = new WP_Query(array('pagename' => 'glosario') );
+	$glosario_id = $query_glosario->get_queried_object_id();
+	$glosario = get_field('glosario', $glosario_id);
 
-<!-- GLOSARIO -->
+	$img_data_glosario = get_field('imagen', $glosario_id);
+	$img_url_glosario = $img_data_glosario['sizes']['img-glosario'];
+	$img_width_glosario = $img_data_glosario['sizes']['img-glosario-width'];
+	$img_height_glosario = $img_data_glosario['sizes']['img-glosario-height'];
+	$img_alt_glosario = $img_data_glosario['alt'];
+	// END QUERY GLOSARIO
+	?>	
 
-<?php 
-$the_query = new WP_Query(array('pagename' => 'glosario') );
-$glosario_id = $the_query->get_queried_object_id();
-$glosario = get_field('glosario', $glosario_id);
-
-$img_data_glosario = get_field('imagen', $glosario_id);
-$img_url_glosario = $img_data_glosario['sizes']['img-glosario'];
-$img_width_glosario = $img_data_glosario['sizes']['img-glosario-width'];
-$img_height_glosario = $img_data_glosario['sizes']['img-glosario-height'];
-$img_alt_glosario = $img_data_glosario['alt'];
-?>	
-
-<div class="content-wrapper">
-
-<section>
-	<div>
-<h3 class="title title--left"><span class="title__span title__span--grey"></span>Glosario</h3>
-
-
-		<div class="table">
-			<h6 class= "table__title-1">Término</h6>
-			<h6 class= "table__title-2">Abreviatura</h6>
-			<h6 class= "table__title-3">Descripción</h6>
-			<p class= "table__item-1 table__item-1--nobg"><?php echo $glosario[0]['termino']?></p>
-			<p class= "table__item-2 table__item-1--nobg"><?php echo $glosario[0]['abreviatura'];?></p>
-			<p class= "table__item-3 table__item-1--nobg"><?php echo $glosario[0]['descripcion'];?></p>
-		</div> 
-
-		<a href= "<?php echo home_url('/glosario') ?>"><button class= "button button--grey">Ver más</button></a>
-
-		<div class="glosario-wrapper">
-		<img class= "glosario__img" src='<?php echo $img_url_glosario; ?> ' width= " <?php echo $img_width_glosario;?>" height= '<?php echo $img_height_glosario; ?>' alt= '<?php echo $img_alt_glosario ?>'
-		</div>
-
-	</div>
-</section>		
-
-<!-- ORGANISMOS Y PAGS UTILES  -->
-
-</div>
-
-<section>
-	<div class="section-wrapper--02">
-		<div class="content-wrapper section-wrapper--flex section-wrapper--flex">
-			<div class="section-wrapper--flex section-wrapper--flex--col">
-				<h3 class="title title--left"><span class="title__span title__span--green"></span>Organismos</h3>
-				<p>Consultá los organismos Nacionales y Provinciales....</p>
-				<a href= "<?php echo home_url('/organismos') ?>"><i class="bi bi-plus"></i></a>
-			</div>
-			<div class="section-wrapper--flex section-wrapper--flex--col">
-				<h3 class="title title--left"><span class="title__span title__span--green"></span>Otras páginas útiles</h3>
-				<p>Encontrá otras páginas y aplicaciones útiles</p>
-				<a href= "<?php echo home_url('/paginas-utiles') ?>"><i class="bi bi-plus"></i></a>
-			</div>
-		</div>
-	</div>
-</section>
-
-
-<div class="content-wrapper">
-
-<section>
-<div class="section-wrapper-censo">
-<h3 id= "anchor-censo" class="title title--left"><span class="title__span title__span"></span>Podés brindar datos para colaborar con el censo</h3>
-<div class="form-wrapper">
-		<div class="form__content--50w">
-			<?php the_field('texto_censo'); ?>
-		</div>
-		<?php echo do_shortcode('[wpforms id="213" title="false" description= "false"]');?>
-		</div>
-		</div>
-</section>
-
-
-<section id="anchor-madres">
-</div>
-	<div class="form-wrapper--bg">
+	<!-- CONTENT WRAPPER -->
 	<div class="content-wrapper">
-	<h3 class="title title--left title--white"><span class="title__span title__span"></span>Si buscás a un hijo o hija, vos también podés iniciar la búsqueda</h3>
-	<div class="form-wrapper form-wrapper--reverse form-wrapper--white">
-		<div class="form__content--50w">
-			<p class="form__text--white"><?php the_field('texto_madres'); ?></p>
+		<!-- SECTION GLOSARIO -->
+		<section class= "mt">
+			<h3 class="title title--left"><span class="title__span title__span--grey"></span>Glosario</h3>
+			<!-- TABLE -->
+			<div class="table">
+				<h6 class= "table__title-1">Término</h6>
+				<h6 class= "table__title-2">Abreviatura</h6>
+				<h6 class= "table__title-3">Descripción</h6>
+				<p class= "table__item-1 table__item-1--nobg"><?php echo $glosario[0]['termino']?></p>
+				<p class= "table__item-2 table__item-1--nobg"><?php echo $glosario[0]['abreviatura'];?></p>
+				<p class= "table__item-3 table__item-1--nobg"><?php echo $glosario[0]['descripcion'];?></p>
+			</div> 
+			<!-- END TABLE -->
+			<a href= "<?php echo home_url('/glosario') ?>"><button class= "button button--grey">Ver más</button></a>
+			<!-- IMG GLOSARIO -->
+			<div class="glosario-wrapper">
+				<img class= "glosario__img" src='<?php echo $img_url_glosario; ?> ' width= " <?php echo $img_width_glosario;?>" height= '<?php echo $img_height_glosario; ?>' alt= '<?php echo $img_alt_glosario ?>'>
+			</div>
+			<!-- END IMG GLOSARIO -->
+		</section>		
+		<!-- END SECTION GLOSARIO -->
+	</div>
+	<!-- END CONTENT WRAPPER -->
+
+	<!-- SECTION ORGANISMOS Y PAGS UTILES  -->
+	<section>
+		<!-- SECTION BACKGROUND -->
+		<div class="section-bg--02">
+			<!-- CONTENT WRAPPER -->
+			<div class="content-wrapper section--flex">
+				<!-- COLUMNA ORGANISMOS -->
+				<div class="section--flex section--flex--col">
+					<h3 class="title title--left"><span class="title__span title__span--green"></span>Organismos</h3>
+					<p>Consultá los organismos Nacionales y Provinciales....</p>
+					<a href= "<?php echo home_url('/organismos') ?>"><i class="bi bi-plus"></i></a>
+				</div>
+				<!-- END COLUMNA ORGANISMOS -->
+				<!-- COLUMNA PAGS UTILES -->
+				<div class="section--flex section--flex--col">
+					<h3 class="title title--left"><span class="title__span title__span--green"></span>Otras páginas útiles</h3>
+					<p>Encontrá otras páginas y aplicaciones útiles</p>
+					<a href= "<?php echo home_url('/paginas-utiles') ?>"><i class="bi bi-plus"></i></a>
+				</div>
+				<!-- END COLUMNA PAGS UTILES -->
+			</div>
+			<!-- END CONTENT WRAPPER -->
 		</div>
-		<?php echo do_shortcode('[wpforms id="227" title="false" description= "false"]');?>
-</section>
+		<!-- END SECTION BACKGROUND -->
+	</section>
+	<!-- END SECTION ORGANISMOS Y PAGS UTILES -->
 
+	<!-- CONTENT WRAPPER -->
+	<div class="content-wrapper">
+	<!-- SECTION FORM CENSO -->
+		<section>
+				<h3 id= "anchor-censo" class="title title--left"><span class="title__span title__span"></span>Podés brindar datos para colaborar con el censo</h3>
+				<!-- FORM WRAPPER -->
+				<div class="form-wrapper">
+					<!-- FORM DESCRIPTION -->
+					<div class="form__content--50w">
+						<?php the_field('texto_censo'); ?>
+					</div>
+					<!-- END FORM DESCRIPTION -->
+					<?php echo do_shortcode('[wpforms id="213" title="false" description= "false"]');?>
+				</div>
+				<!-- END FORM WRAPPER -->
+		</section>
+	<!-- END SECTION FORM CENSO -->
 	</div>
-	</div>
-	</div>
+	<!-- END CONTENT WRAPPER -->
+	
+	<!-- SECTION FORM MADRES -->
+	<section id="anchor-madres" class= "mt--5">
+		<!-- SECTION BACKGROUND -->
+		<div class="form-wrapper--bg">
+			<!-- CONTENT WRAPPER -->
+			<div class="content-wrapper">
+				<h3 class="title title--left title--white"><span class="title__span title__span"></span>Si buscás a un hijo o hija, vos también podés iniciar la búsqueda</h3>
+				<!-- FORM WRAPPER -->
+				<div class="form-wrapper form-wrapper--reverse form-wrapper--white">
+					<!-- FORM DESCRIPTION -->
+					<div class="form__content--50w">
+						<p class="form__text--white"><?php the_field('texto_madres'); ?></p>
+					</div>
+					<!-- END FORM DESCRIPTION -->
+					<?php echo do_shortcode('[wpforms id="227" title="false" description= "false"]');?>
+				</div>
+				<!-- END FORM WRAPPER -->
+			</div>
+			<!-- END CONTENT WRAPPER -->
+		</div>
+		<!-- END SECTION BACKGROUND -->
+	</section>
+	<!-- END SECTION FORM MADRES -->
+</main>
+<!-- END MAIN -->
 
-
-<div class= "content-wrapper">
-
-	</main><!-- #main -->
-	</div>
-    <?php 	get_footer();?>
+<?php 	get_footer();?>
