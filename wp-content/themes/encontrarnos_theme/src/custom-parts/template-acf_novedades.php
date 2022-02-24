53e6526e1a10d73a49ddfe2a5f5d95d506d17702
María Belén Trigueros <bel.trigueros@gmail.com>
2022-02-23 23:30:02 -0300

    Update novedades

--- wp-content/themes/encontrarnos_theme/src/custom-parts/template-acf_novedades.php
+++ wp-content/themes/encontrarnos_theme/src/custom-parts/template-acf_novedades.php
@@ -1,23 +1,24 @@
-
 <?php if(have_rows('links')): ?>
     <?php while(have_rows('links')): the_row(); ?>
-
     <?php 
+    // VARIABLES CARD
     $img_data = get_sub_field('imagen');
     $img_url = $img_data['sizes']['card-img'];
     $img_width = $img_data['sizes']['card-img-width'];
     $img_height = $img_data['sizes']['card-img-height'];
     $img_alt = $img_data['alt'];
     ?> 
-
     <?php $link = get_sub_field('link'); ?>
-
-    <div class="card">
-    <img class= 'card__img' src= '<?php echo $img_url; ?> ' width= " <?php echo $img_width;?>" height= '<?php echo $img_height ; ?>' alt= '<?php echo $img_alt ?>'>
+    <!-- CARD -->
+    <div class="card">    
+        <img class= 'card__img' src= '<?php echo $img_url; ?> ' width= " <?php echo $img_width;?>" height= '<?php echo $img_height ; ?>' alt= '<?php echo $img_alt ?>'>
+        <!-- CARD TEXT WRAPPER	 -->
         <div class="card__text-wrapper">
-           <a href=" <?php echo $link['url'];?>">  <h5 class= "card__title card__title--left"> <?php echo $link['title'];?> </h5></a>
+            <a href=" <?php echo $link['url'];?>">  <h5 class= "card__title card__title--left"> <?php echo $link['title'];?> </h5></a>
             <p class= "card__text card__text--left"> <?php the_sub_field('descripcion')?></p>
         </div>
+        <!-- END CARD TEXT WRAPPER -->
 	</div>
+    <!-- END CARD -->
     <?php endwhile; ?>
-    <?php endif; ?>
+<?php endif; ?>
--- wp-content/themes/encontrarnos_theme/template-novedades.php
+++ wp-content/themes/encontrarnos_theme/template-novedades.php
@@ -7,51 +7,58 @@ get_header();
 
 </div>
 </div>
-
-
+<!-- END CONTENT WRAPPER -->
 
 <?php 
+
+// VARIABLES HERO
 $img_data = get_field('imagen-n', 'options');
 $img_url = $img_data['sizes']['hero-img'];
 $img_width = $img_data['sizes']['hero-img-width'];
 $img_height = $img_data['sizes']['hero-img-height'];
 $img_alt = $img_data['alt'];
+
 ?> 
 
-	
+<!-- HERO -->
+
+<!-- HERO BACKGROUND  -->
 <div class="hero__bg">
-<div class="content-wrapper hero">
-<div class="hero__text">
-<h1 class= "title title--left"><?php the_field('titulo-n', 'options'); ?></h1>
-<?php the_field('descripcion-n', 'options'); ?>
+	<!-- HERO WRAPPER  -->
+	<div class="content-wrapper hero">
+			<!-- HERO TEXT WRAPPER  -->
+		<div class="hero__text">
+			<h1 class= "title title--left"><?php the_field('titulo-n', 'options'); ?></h1>
+			<?php the_field('descripcion-n', 'options'); ?>
+		</div>
+		<!-- END HERO TEXT WRAPPER -->
+		<img class= 'hero__img' src= '<?php echo $img_url; ?> ' width= " <?php echo $img_width;?>" height= '<?php echo $img_height ; ?>' alt= '<?php echo $img_alt ?>'>
+	</div>
+	<!-- END HERO WRAPPER -->
 </div>
+<!-- END HERO BACKGROUND -->
 
-<img class= 'hero__img' src= '<?php echo $img_url; ?> ' width= " <?php echo $img_width;?>" height= '<?php echo $img_height ; ?>' alt= '<?php echo $img_alt ?>'>
-
-
-</div>
-</div>
+<!-- CONTENT WRAPPER -->
 <div class="content-wrapper section-novedades">
-
-
+	<!-- MAIN -->
 	<main id="primary" class="site-main">
-			<section class="card-wrapper section-novedades">
-			<?php if ( have_posts() ) : ?>
-				<header class="page-header"></header><!-- .page-header -->
-			<?php
-				/* Start the Loop */
-			while ( have_posts() ) : the_post();?>
-						<?php get_template_part( './src/custom-parts/template', 'acf_novedades' );?>
-
-				<?php endwhile;
-			else :
+		<!-- CARD WRAPPER -->
+		<section class="card-wrapper">
+			<!-- THE LOOP -->
+			<?php if ( have_posts() ) : while ( have_posts() ) : the_post();?>
+				<?php get_template_part( './src/custom-parts/template', 'acf_novedades' );?>
+				<?php endwhile; else :
 				get_template_part( 'template-parts/content', 'none' );
 			endif; ?>
-	</section>
+			<!-- END LOOP -->
+		</section>
+		<!-- END CARD WRAPPER -->
+	</main>
+	<!-- END MAIN -->
+</div>
+<!-- END CONTENT WRAPPER -->
 	
-	</div>
-	
-	<?php
-	get_footer();
+<?php
+get_footer();
 
 
